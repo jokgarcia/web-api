93afd76e487f718f99e0b96756b55e27ea09dab4
CRA <christian.r.arellano@gmail.com>
2019-07-20 16:43:42 +0800

    EditEmployeeRepo

--- EmployeeManagement/DataAccessLayer/Repository/EmployeeRepository.cs
+++ EmployeeManagement/DataAccessLayer/Repository/EmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,35 @@ namespace DataAccessLayer.Repository
 {
     class EmployeeRepository
     {
+        public bool AddEmployee()
+        {
+
+            return true;
+        }
+        public bool AddEmployee(Employee employee)
+        {
+            return true;
+        }
+        public IList<Employee> GetEmployees()
+        {
+            var employees = new List<Employee>();
+            var employee = new Employee()
+            {
+
+            };
+
+
+            return employees;
+        }
+        public bool UpdateEmployee()
+        {
+
+            return true;
+        }
+        public bool DeleteEmployee()
+        {
+
+            return true;
+        }
     }
 }
