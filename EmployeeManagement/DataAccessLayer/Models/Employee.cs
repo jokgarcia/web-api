e5b80476b61778bfff5a223bf667571c3db94459
GodPrince <GodPrince@DESKTOP-PN7HPFF>
2019-07-20 17:54:48 +0900

    Modified Model Employee

--- EmployeeManagement/DataAccessLayer/Models/Employee.cs
+++ EmployeeManagement/DataAccessLayer/Models/Employee.cs
@@ -6,7 +6,9 @@ namespace DataAccessLayer.Models
 {
     public class Employee
     {
-        public int MyProperty { get; set; }
-        public int Number { get; set; }
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
     }
 }
