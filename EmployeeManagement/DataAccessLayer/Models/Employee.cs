fb1a0bbbb179356a9d134051233599f7d69fc579
Garcia, Simeon Y <simeon.y.garcia@accenture.com>
2019-07-20 15:43:35 +0800

    Jok

---
+++ EmployeeManagement/DataAccessLayer/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Models
+{
+    public class Employee
+    {
+        public int MyProperty { get; set; }
+        public int Number { get; set; }
+    }
+}
