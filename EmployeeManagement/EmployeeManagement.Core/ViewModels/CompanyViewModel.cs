142ae0dec5131914d3f2cd55f15590140bc8fd67
florence escano <florenceescano@gmail.com>
2019-08-18 17:41:12 +0800

    company 3

---
+++ EmployeeManagement/EmployeeManagement.Core/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagement.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Core.ViewModels
+{
+    public class CompanyViewModel
+    {
+     public Company company { get; set; }
+
+    }
+}
